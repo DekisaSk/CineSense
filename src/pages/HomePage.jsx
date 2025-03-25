2fe3797bf361e711c7429208c7a7de3a7d71e81d
Andrija Puletic <andrija.puletic@gmail.com>
2025-03-25 13:51:51 +0100

    [CNSN-41] - Minor restyling of the h4 titles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,17 +3,26 @@ import MovieSlider from "C:/Users/andri/Coinis Project/CineSense-1/src/component
 import TopRatedContainer from "../components/TopRatedContainer";
 import BestMovieContainer from "../components/BestMovieContainer";
 export default function HomePage() {
+  const apiKey = "d9bb4067";
+
+  // fetch(`https://www.omdbapi.com/?s=inception&apikey=${ap}&page=1`)
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //   })
+  //   .catch((error) => console.error("Error:", error));
+
   return (
     <>
       <MovieSlider></MovieSlider>
 
       <div className="home-container">
         <div className="left-section">
-          <h4>Top Rated:</h4>
+          <h4 className="title">Top Rated:</h4>
           <TopRatedContainer></TopRatedContainer>
         </div>
         <div className="right-section">
-          <h4>Most Popular:</h4>
+          <h4 className="title">Most Popular:</h4>
           <BestMovieContainer></BestMovieContainer>
         </div>
       </div>
