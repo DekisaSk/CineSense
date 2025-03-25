b7d13cbaf037a78d4ebb77c8f5bc16f25231dffd
Dejan Skiljic <skiljic.dejan@gmail.com>
2025-03-25 02:26:10 +0100

    [CNSN-37] - Cleaned up routing by creating MainRoutes component with lazy loading

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,13 @@ import App from "./App.jsx";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage.jsx";
-import MoviesPage from "./pages/MoviesPage.jsx";
-import AboutPage from "./pages/AboutPage.jsx";
-import CustomPage from "./pages/CustomPage.jsx";
+import MainRoutes from "./router/MainRoutes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/list" element={<MoviesPage />} />
-        <Route path="/custom" element={<CustomPage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
+      <MainRoutes />
       <App />
       <Footer />
     </BrowserRouter>
