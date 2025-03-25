23fcd34ac4c50846a8f8fb79e32f848d6acb3ef4
Dejan Skiljic <skiljic.dejan@gmail.com>
2025-03-25 01:31:57 +0100

    [CNSN-36] - Added Footer component

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
 import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Header />
     <App />
+    <Footer />
   </StrictMode>
 );
